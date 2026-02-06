b5a625f5c5f9c35e9dfce4fadacdde5fb0e3d3ee
Chuqing Zhang <ashley@Chuqings-MacBook-Pro-2233.local>
2026-02-06 12:47:33 -0500

    2nd version of hello world

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -4,6 +4,8 @@ public class HelloGitHub {
     public static void main(String[] args) {
         // TODO Write your code here
         System.out.println("Hello, world!");
+        System.out.println("你好！");
+        System.out.println("Hola, world!");
         
 
     }
